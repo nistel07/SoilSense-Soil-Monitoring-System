f925df09da5605d17a895e9dfafdafc3e438c3b2
Your Name <you@example.com>
2025-03-23 21:07:15 +0530

    ok

--- app.js
+++ app.js
@@ -95,8 +95,8 @@ app.use(function (err, req, res, next) {
 });
 
 // Start Server
-server.listen(8000, "0.0.0.0", () => {
-  console.log("Server running on http://localhost:8000");
+server.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
 });
 
 module.exports = { app, server, io };
